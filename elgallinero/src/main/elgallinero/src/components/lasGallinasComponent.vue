dfd1152ff58477259f7b8ca876050e708754998f
Xavi1594 <xavi9415@hotmail.com>
2023-04-19 18:16:08 +0200

    cards añadidas

---
+++ elgallinero/src/main/elgallinero/src/components/lasGallinasComponent.vue
@@ -0,0 +1,60 @@
+<script setup>
+</script>
+<template>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-4 g-4 mt-3">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../assets/img/mikel.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Vasco, del Barça y vive en Alemania, dificil empeorarlo</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../assets/img/juan.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Apasionado de la cultura inglesa y del balconing</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../assets/img/kevin.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Es de Graná pero se creé de Belo Horizonte</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../assets/img/hector.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">No me sale ni trolearle, es un crack</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../assets/img/pablo.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Más laportista que Laporta</p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="col">
+                <div class="card h-100">
+                    <img src="../assets/img/pablo.jpg" class="card-img-top" alt="...">
+                    <div class="card-body text-center">
+                        <span class="h4 ">Mr Kumani</span>
+                        <p class="card-text">Koeman, perlas, fútbol pirata, in that order</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
--- elgallinero/src/main/elgallinero/src/views/LasGallinasView.vue
+++ elgallinero/src/main/elgallinero/src/views/LasGallinasView.vue
@@ -1,7 +1,8 @@
 <script setup>
 import navbarComponent from '../components/navbarComponent.vue';
+import lasGallinasComponent from '../components/lasGallinasComponent.vue';
 </script>
 <template>
     <navbarComponent />
-
+<lasGallinasComponent />
 </template>
