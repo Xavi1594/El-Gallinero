cb9afa7f31e6b5530793bd602a58776d1d69086c
Xavi1594 <xavi9415@hotmail.com>
2023-04-20 21:32:21 +0200

    user repository

--- elgallinero/src/main/java/elgallinero/elgallinero/repositories/UserRepository.java
+++ elgallinero/src/main/java/elgallinero/elgallinero/repositories/UserRepository.java
@@ -1,5 +1,11 @@
 package elgallinero.elgallinero.repositories;
 
-public interface UserRepository {
-    
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import elgallinero.elgallinero.models.User;
+
+public interface UserRepository extends JpaRepository <User,Long> {
+    public Optional<User> findByUsername(String username);
 }
